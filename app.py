e895c96b812617e417db1ecba5683281eb348102
sai8151 <saikiranreddy8151@gmail.com>
2023-10-23 01:18:02 +0530

    #132

--- app.py
+++ app.py
@@ -1,80 +1,82 @@
-import PyPDF2
+from flask import Flask, render_template, request, send_file
+from gensim.summarization import summarize
+import pdfplumber
 from pptx import Presentation
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.probability import FreqDist
-from heapq import nlargest
-from transformers import pipeline
+from pptx.util import Inches
+import tempfile
+import random
 
-# extract text from pdf
-def extract_text_from_pdf(pdf_file_path):
-    text = ""
-    try:
-        with open(pdf_file_path, 'rb') as pdf_file:
-            pdf_reader = PyPDF2.PdfReader(pdf_file)
-            for page_num in range(len(pdf_reader.pages)):
-                page = pdf_reader.pages[page_num]
-                text += page.extract_text()
-        return text
-    except Exception as e:
-        print(f"Error reading PDF: {str(e)}")
-        return None
+app = Flask(__name__)
 
-# Function to generate a PowerPoint presentation (Placeholder)
-def generate_ppt_from_text(text_content):
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.route('/summarize', methods=['POST'])
+# def summarize_text():
+#     text = request.form['text']
+#     summary = summarize(text)
+#     return render_template('summary.html', summary=summary, original_text=text)
+
+@app.route('/upload', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+
+        # Set the path to your PDF file and the output PPTX file in a temporary directory
+        pdf_path = f.filename
+        output_pptx_path = tempfile.mktemp(suffix=".pptx")
+
+        create_ppt_from_summarized_content(pdf_path, output_pptx_path)
+
+        return send_file(output_pptx_path, as_attachment=True, download_name="output.pptx")
+
+def extract_headings_and_content(pdf_path):
+    headings_and_content = []
+    with pdfplumber.open(pdf_path) as pdf:
+        for page_num in range(len(pdf.pages)):
+            page = pdf.pages[page_num]
+            page_text = page.extract_text()
+            if page_text:
+                lines = page_text.split('\n')
+                headings = []
+                content = []
+                for line in lines:
+                    if line.isupper() or line.startswith('Chapter') or line.startswith('Section'):
+                        headings.append(line)
+                    else:
+                        content.append(line)
+                if headings:
+                    headings_and_content.append((headings, content))
+    return headings_and_content
+
+def create_ppt_from_summarized_content(pdf_path, output_pptx_path):
+    headings_and_content = extract_headings_and_content(pdf_path)
     presentation = Presentation()
-    for section in text_content.split('\n\n'):
-        slide = presentation.slides.add_slide(presentation.slide_layouts[1])
-        slide.shapes.title.text = section[:100]  # Use the first 100 characters as slide title
-        slide.shapes.add_textbox(left=100, top=100, width=500, height=300).text = section
-    presentation.save('output.pptx')
+    
+    # Define the maximum number of slides to generate
+    max_slides = 5  # You can adjust this as needed
+    
+    # Shuffle the order of content for randomness
+    random.shuffle(headings_and_content)
+    
+    for i, (headings, content) in enumerate(headings_and_content):
+        if i >= max_slides:
+            break  # Stop after generating the maximum number of slides
+        
+        slide = presentation.slides.add_slide(presentation.slide_layouts[5])  # Blank slide layout
+        if headings:
+            title = slide.shapes.title
+            title.text = headings[0]  # Use the first heading as the slide title
+        if content:
+            word_count = random.randint(100, 150)
+            summarized_content = summarize("\n".join(content), word_count=word_count)            
+            text_box = slide.shapes.add_textbox(Inches(1), Inches(1), Inches(8), Inches(5))
+            text_frame = text_box.text_frame
+            p = text_frame.add_paragraph()
+            p.text = summarized_content
+    presentation.save(output_pptx_path)
 
-# Function to integrate YouTube video links (Placeholder)
-def integrate_youtube_videos(text_content):
-    # Placeholder for YouTube integration code
-    pass
-
-def generate_summary(text_content, percentage=0.8):
-    # Tokenize the text into sentences and words
-    sentences = sent_tokenize(text_content)
-    words = word_tokenize(text_content)
-    
-    # Remove stopwords and punctuation
-    stop_words = set(stopwords.words('english'))
-    words = [word.lower() for word in words if word.isalnum() and word.lower() not in stop_words]
-    
-    # Calculate word frequencies
-    word_freq = FreqDist(words)
-    
-    # Calculate the score for each sentence based on word frequency
-    sentence_scores = {}
-    for sentence in sentences:
-        for word in word_tokenize(sentence):
-            if word.lower() in word_freq:
-                if sentence not in sentence_scores:
-                    sentence_scores[sentence] = word_freq[word.lower()]
-                else:
-                    sentence_scores[sentence] += word_freq[word.lower()]
-    
-    # Calculate the desired number of sentences for the summary based on the percentage
-    num_sentences = int(len(sentences) * percentage)
-    
-    # Get the top 'num_sentences' sentences with the highest scores
-    summary_sentences = nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
-    
-    # Combine the selected sentences to generate the summary
-    summary = ' '.join(summary_sentences)
-    summarizer = pipeline("summarization")
-    summary = summarizer(text_content, max_length=150, min_length=30, do_sample=False)[0]['summary_text']
-    return summary
-
-# Example usage
-pdf_file_path = 'eeen101.pdf'
-text_content = extract_text_from_pdf(pdf_file_path)
-
-if text_content:
-    generate_ppt_from_text(text_content)
-    integrate_youtube_videos(text_content)
-    summary = generate_summary(text_content)
-    print(summary)
+if __name__ == '__main__':
+    app.run(debug=True)
